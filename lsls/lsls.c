de54fb68cdd3d82e59483a3514a7f22a640763c2
Civoremo <omerovic_n@hotmail.com>
2019-04-26 13:24:53 -0400

    lsls

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+
 
 /**
  * Main
@@ -7,27 +9,55 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-  char *commandNum = argc;
-  char *inputPath = argv[1];
+  int opt;
 
-  if (inputPath == NULL) {
-    printf("ITS EMPTY\n");
-  } else {
-    printf("WE HAVE SOMETHING\n");
+  for (opt = 0; opt < argc; opt++) {
+    // printf("%s\n", argv[opt]);
   }
 
   // Open directory
-  struct dirent *pathDir;
-  DIR *searchPath;
-  DIR *defaultPath;
+  struct dirent *pDirent;
+  DIR *our_path;
+  DIR *default_path;
+  
+  our_path = opendir(argv[1]);
+  default_path = opendir(".");
 
-  searchPath = opendir(inputPath);
-  defaultPath = opendir(".");
+  struct stat buf;
 
   // Repeatly read and print entries
-  
+  if (our_path == NULL) {
+    while ((pDirent = readdir(default_path)) != NULL) {
+      stat(pDirent->d_name, &buf);
+      if ((buf.st_mode & S_IFMT) == S_IFDIR) {
+        printf("<DIR> ");
+        printf("%s\n", pDirent->d_name);
+      } else if((buf.st_mode & S_IFMT) == S_IFREG) {
+        printf("%10ld ", buf.st_size);
+        printf("%s\n", pDirent->d_name);
+      }
+    }
+  } else {
+    while ((pDirent = readdir(our_path)) != NULL) {
+      stat(pDirent->d_name, &buf);
+      if (buf.st_size >= 0) {
+        if ((buf.st_mode & S_IFMT) == S_IFREG) {
+          printf("%10ld ", buf.st_size);
+          printf("%s\n", pDirent->d_name);
+        } else if ((buf.st_mode & S_IFMT) == S_IFDIR) {
+          printf("<DIR> ");
+          printf("%s\n", pDirent->d_name);
+        } 
+      } else {
+        fprintf(stderr, "ST_SIZE ERROR\n");
+      }
+    }
+  }
+
 
   // Close directory
+  closedir(our_path);
+  closedir(default_path);
 
   return 0;
 }
