684168c24e0caffb35ea0b0042e2c064182ff134
Civoremo <omerovic_n@hotmail.com>
2019-04-26 11:21:13 -0400

    parse and open dir

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,10 +7,25 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *commandNum = argc;
+  char *inputPath = argv[1];
+
+  if (inputPath == NULL) {
+    printf("ITS EMPTY\n");
+  } else {
+    printf("WE HAVE SOMETHING\n");
+  }
 
   // Open directory
+  struct dirent *pathDir;
+  DIR *searchPath;
+  DIR *defaultPath;
+
+  searchPath = opendir(inputPath);
+  defaultPath = opendir(".");
 
   // Repeatly read and print entries
+  
 
   // Close directory
 
